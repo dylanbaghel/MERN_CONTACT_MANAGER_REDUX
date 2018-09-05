417877a96ffc54def6f04b4c59841af0227cc60a
dylanbaghel <baghelabhi39@gmail.com>
2018-09-06 00:23:07 +0530

    Setup For Heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('./config/config');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 //CUSTOM MODULES FILES
 const { mongoose } = require('./db/mongoose');
@@ -14,6 +15,16 @@ app.use(cors());
 
 
 app.use('/api/contacts', contacts);
+//SERVE REACT STATIC FRONTEND
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client', 'public')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
+    });
+}
+
+
 //LISTEN
 app.listen(process.env.PORT, () => {
     console.log(`Server At ${process.env.PORT}`);
